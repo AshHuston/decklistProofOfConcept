0435b13cde0c7234bc3051017a4b94a0d460aa2a
Ash Huston <35748045+AshHuston@users.noreply.github.com>
2026-04-02 10:12:04 -0400

    initial commit

--- decklistpoc/src/main.js
+++ decklistpoc/src/main.js
@@ -5,15 +5,3 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-
-const express = require('express')
-const app = express()
-const port = 4000 
-
-app.get('/', (req, res) => {
-  res.send(App)
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
