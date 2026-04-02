f3465de23ed08f93cb4076908a2fd54de327195b
Ash Huston <35748045+AshHuston@users.noreply.github.com>
2026-04-02 10:52:23 -0400

    initial commit

--- decklistpoc/app.js
+++ decklistpoc/app.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import express from 'express';
 import { fileURLToPath } from 'url';
+
+
 const app = express()
 const port = 4000 
 
@@ -10,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../../dist')));
 
 // Catch-all: send index.html for client-side routing
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, () => {
